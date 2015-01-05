a293642a71e90a7f7bb770ce2918837cca0f1f55
Ruben de Vries <ruben@rubensayshi.com>
2015-01-05 18:49:37 +0100

    cleaned up examples a bit

--- examples/simple_webhooks_setup_example.php
+++ examples/simple_webhooks_setup_example.php
@@ -4,7 +4,7 @@ use Blocktrail\SDK\BlocktrailSDK;
 
 require_once __DIR__ . "/../vendor/autoload.php";
 
-$client = new BlocktrailSDK("MY_APIKEY", "MY_APISECRET", "BTC", true, 'v1');
+$client = new BlocktrailSDK("MY_APIKEY", "MY_APISECRET", "BTC", true /* testnet */, 'v1');
 
 $client->setupWebhook("http://rubensayshi.ngrok.com/webhook", "rubensayshi-ngrok");
 $client->subscribeAddressTransactions("rubensayshi-ngrok", "2N6Fg6T74Fcv1JQ8FkPJMs8mYmbm9kitTxy");
