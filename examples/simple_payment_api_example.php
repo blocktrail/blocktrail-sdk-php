2091dc817032988ef98b855632611a1a4bb7db5b
Ruben de Vries <ruben@rubensayshi.com>
2015-01-05 18:49:37 +0100

    cleaned up examples a bit

--- examples/simple_payment_api_example.php
+++ examples/simple_payment_api_example.php
@@ -5,8 +5,8 @@ use Blocktrail\SDK\Connection\Exceptions\ObjectNotFound;
 
 require_once __DIR__ . "/../vendor/autoload.php";
 
-$client = new BlocktrailSDK("MY_APIKEY", "MY_APISECRET", "BTC", true, 'v1');
-$client->setVerboseErrors();
+$client = new BlocktrailSDK("MY_APIKEY", "MY_APISECRET", "BTC", true /* testnet */, 'v1');
+// $client->setVerboseErrors();
 // $client->setCurlDebugging();
 
 /**
@@ -14,23 +14,20 @@ $client->setVerboseErrors();
  * @var $backupMnemonic     string
  */
 try {
-    $wallet = $client->initWallet("rubensayshi3", "password");
+    $wallet = $client->initWallet("example-wallet", "example-strong-password");
 } catch (ObjectNotFound $e) {
-    list($wallet, $backupMnemonic) = $client->createNewWallet("rubensayshi3", "password", 9999); // 9999 = use blocktrail development key
-    // var_dump($wallet->doDiscovery());
-    var_dump($wallet->getNewAddress(), $wallet->getNewAddress(), $wallet->getNewAddress(), $wallet->getNewAddress());
-    /*
-        string(35) "2N31kiJ7JR7VoMf1La3xyNHMPni2Xh33FK7"
-        string(35) "2N3F17VrrVxvvb1x1K4zfwLaYuebCf9ZtHp"
-        string(35) "2N6Fg6T74Fcv1JQ8FkPJMs8mYmbm9kitTxy"
-        string(35) "2N4xQKufeRWx7n15FPE8ziJrjfrKe3Gw3TT"
-     */
+    list($wallet, $backupMnemonic) = $client->createNewWallet("example-wallet", "example-strong-password");
 }
 
+// print some new addresses
+var_dump($wallet->getNewAddress(), $wallet->getNewAddress());
+
+// print the balance
 list($confirmed, $unconfirmed) = $wallet->getBalance();
 var_dump($confirmed, BlocktrailSDK::toBTC($confirmed));
 var_dump($unconfirmed, BlocktrailSDK::toBTC($unconfirmed));
 
+// send a payment (will fail unless you've send some BTC to an address part of this wallet)
 var_dump($wallet->pay([
     "2N6Fg6T74Fcv1JQ8FkPJMs8mYmbm9kitTxy" => BlocktrailSDK::toSatoshi(0.001)
 ]));
