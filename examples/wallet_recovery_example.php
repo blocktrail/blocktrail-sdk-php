d786707a11f88b5984f18952d22ed162711b626a
Oisin <oisin@blocktrail.com>
2015-02-04 14:03:32 +0100

    testing backup data and logging

--- examples/wallet_recovery_example.php
+++ examples/wallet_recovery_example.php
@@ -10,28 +10,33 @@ require_once __DIR__ . "/../vendor/autoload.php";
 set_time_limit(0);
 
 //the primary mnemonic, obtained from our backup pdf
-$primaryMnemonic    = "print work mail summer small crouch auto one taste boat spin dog reward
-winter milk onion civil nice symbol bright boy whale fabric cement collect
-voice diagram dress tank warfare skin where brand immune this rescue
-security ahead weasel stay kind tornado butter process promote cheap scheme
-kid";
+$primaryMnemonic    = "craft scatter train family grass bind sense double sample raven luxury
+vacant north limit swamp scatter warm core enable exist limit genius knee
+clutch document metal decline shaft canal utility powder earth later
+symptom movie pluck traffic ozone power evoke spread gym habit legal cruel
+uniform fold high";
 
 //our wallet passphrase
 $primaryPassphrase  = "example-strong-password";
 
 //the primary mnemonic, obtained from our backup pdf
-$backupMnemonic     = "toddler green make maple gas slush chest tag dutch employ noodle fade
-borrow carry pupil argue deer shadow attack escape unfair worry shuffle
-regular local away shrimp amount endless venue practice decade author
-trouble elite view squirrel alone swear squirrel latin stable fat window
-express diesel design cart";
+$backupMnemonic     = "evil pill forum vault pelican method clever dwarf hurt morning survey core
+teach culture parade slam fee where flower climb silk piano ribbon pelican
+candy genius battle entire lawn topic embark scheme heavy tackle proof
+solution mimic crop average drama jar lava announce toy copper maid reduce
+hand";
 
 //the blocktrail keys used by this wallet, obtained from the backup pdf
 $blocktrailKeys = array(
     [
-        'keyIndex'=> 9999,
-        'path' => "M/9999'",
-        'pubkey' => 'tpubD9q6vq9zdP3gbhpjs7n2TRvT7h4PeBhxg1Kv9jEc1XAss7429VenxvQTsJaZhzTk54gnsHRpgeeNMbm1QTag4Wf1QpQ3gy221GDuUCxgfeZ',
+        'keyIndex'=> 0,
+        'path' => "M/0'",
+        'pubkey' => 'tpubD8UrAbbGkiJUmxDS9UxC6bvSGVd1vAEDMMkMBHTJ7xMMnkNuvBsVMQv6fXxAQgV3aaETetdaBBNQgULBzebM86MyYP526Ggqu8K8jPwBdP4',
+    ],
+    [
+    'keyIndex'=> 9999,
+    'path' => "M/9999'",
+    'pubkey' => 'tpubD9q6vq9zdP3gbhpjs7n2TRvT7h4PeBhxg1Kv9jEc1XAss7429VenxvQTsJaZhzTk54gnsHRpgeeNMbm1QTag4Wf1QpQ3gy221GDuUCxgfeZ',
     ]
 );
 
@@ -40,11 +45,12 @@ $bitcoinClient = new BlocktrailBitcoinService("MY_APIKEY", "MY_APISECRET", "BTC"
 
 //create instance of sweeper - will automatically create primary keys from mnemonics
 $walletSweeper = new WalletSweeper($primaryMnemonic, $primaryPassphrase, $backupMnemonic, $blocktrailKeys, $bitcoinClient, 'btc', $_testnet=true);
+$walletSweeper->enableLogging();    //enable logging for more info
 
 //Do wallet fund discovery - can be run separately from sweeping
 //var_dump($walletSweeper->discoverWalletFunds());
 
 //Do wallet fund discovery and sweeping - if successful you will be returned a transaction ready to submit to the network
-$receivingAddress = "2NDGLXM74jXQRbXUDBiJCSS8ERnD1jgp9hZ";
+$receivingAddress = "2NCcm7hJfJ5wk6GyKvT2ZHCrNsBgjBv2MSF";
 $result = $walletSweeper->sweepWallet($receivingAddress);
 var_dump($result);
