9d8780418c519868dc1fe6399d1877d46b587b30
Ruben de Vries <ruben@rubensayshi.com>
2015-02-12 18:23:51 +0100

    added BackupGenerator to exclude list for now

--- coverage-checker.php
+++ coverage-checker.php
@@ -26,8 +26,9 @@ $totalElements   = 0;
 $checkedElements = 0;
 
 $excludes = [
-    "/^Blocktrail\\\\SDK\\\\Console/",
-    "/Compiler\.php/",
+    "/^Blocktrail\\\\SDK\\\\Console/",  // just wrappers and a pain in the ass to test
+    "/Compiler\\.php/",                 // just a tool and copy paste from Composer
+    "/BackupGenerator\\.php/",          // tmp excluding, it's rather hard to test the whole process
 ];
 
 foreach ($xml->xpath("//package") as $package) {
