62e4a5d85a8a2c75a2ff566efb7e044f707a81fa
Ruben de Vries <ruben@rubensayshi.com>
2016-02-18 10:30:53 +0100

    v1.3.0 user-agent bump

--- src/Blocktrail.php
+++ src/Blocktrail.php
@@ -18,7 +18,7 @@ abstract class Blocktrail {
     const DUST = 546;
     const BASE_FEE = 10000;
 
-    const SDK_VERSION = "1.2.20";
+    const SDK_VERSION = "1.3.0";
     const SDK_USER_AGENT = "blocktrail-sdk-php";
     const DEFAULT_TIME_ZONE = "UTC";
 
