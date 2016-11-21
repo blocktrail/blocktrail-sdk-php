bca3815f0be7ec823f3dce539566b17b3ca641fd
Ruben de Vries <ruben@rubensayshi.com>
2016-11-21 16:38:43 +0100

    NIT

--- src/WalletPath.php
+++ src/WalletPath.php
@@ -102,7 +102,9 @@ class WalletPath {
      * @throws \Exception
      */
     public static function fromBIP32Path(BIP32Path $path) {
-        if (strtolower($path[0]) != "m") throw new \Exception("Path should be absolute [{$path}]");
+        if (strtolower($path[0]) != "m") {
+            throw new \Exception("Path should be absolute [{$path}]");
+        }
 
         return static::create($path[1], $path[2], $path[3]);
     }
