6f655e3756c8c70fc74d6d3e922d600ea59cb180
Thomas Kerin <thomas.kerin@bitmaintech.com>
2017-11-27 14:23:36 +0100

    Extract WalletTestCase as base class for wallet related tests

--- tests/WalletTest.php
+++ tests/WalletTest.php
@@ -11,7 +11,6 @@ use BitWasp\Bitcoin\Address\SegwitAddress;
 use BitWasp\Bitcoin\Key\Deterministic\HierarchicalKeyFactory;
 use BitWasp\Bitcoin\Mnemonic\Bip39\Bip39SeedGenerator;
 use BitWasp\Bitcoin\Script\Classifier\OutputClassifier;
-use BitWasp\Bitcoin\Script\ScriptFactory;
 use BitWasp\Bitcoin\Script\ScriptInterface;
 use BitWasp\Bitcoin\Script\ScriptType;
 use BitWasp\Bitcoin\Script\WitnessProgram;
@@ -21,17 +20,14 @@ use BitWasp\Bitcoin\Transaction\TransactionInterface;
 use BitWasp\Bitcoin\Transaction\TransactionOutput;
 use BitWasp\Buffertools\Buffer;
 
-use Blocktrail\CryptoJSAES\CryptoJSAES;
 use Blocktrail\SDK\Bitcoin\BIP32Key;
 use Blocktrail\SDK\BlocktrailSDK;
-use Blocktrail\SDK\BlocktrailSDKInterface;
 use Blocktrail\SDK\Connection\Exceptions\ObjectNotFound;
 use Blocktrail\SDK\Exceptions\BlocktrailSDKException;
 use Blocktrail\SDK\SignInfo;
 use Blocktrail\SDK\TransactionBuilder;
 use Blocktrail\SDK\Wallet;
 use Blocktrail\SDK\WalletInterface;
-use Blocktrail\SDK\WalletPath;
 use Blocktrail\SDK\WalletScript;
 use Blocktrail\SDK\WalletV1;
 use Blocktrail\SDK\WalletV2;
@@ -48,140 +44,7 @@ use Blocktrail\SDK\WalletV3;
  *
  * @package Blocktrail\SDK\Tests
  */
-class WalletTest extends BlocktrailTestCase {
-    const DEFAULT_WALLET_VERSION = 'v3';
-    const DEFAULT_WALLET_INSTANCE = WalletV3::class;
-
-    /**
-     * @var Wallet[]
-     */
-    protected $wallets = [];
-
-    /**
-     * params are same defaults as BlocktrailSDK, except testnet, which is `true` for these tests
-     * @see BlocktrailTestCase::setupBlocktrailSDK
-     * @return BlocktrailSDK
-     */
-    public function setupBlocktrailSDK($network = 'BTC', $testnet = true, $apiVersion = 'v1', $apiEndpoint = null) {
-        $apiKey = getenv('BLOCKTRAIL_SDK_APIKEY') ?: 'EXAMPLE_BLOCKTRAIL_SDK_PHP_APIKEY';
-        $apiSecret = getenv('BLOCKTRAIL_SDK_APISECRET') ?: 'EXAMPLE_BLOCKTRAIL_SDK_PHP_APISECRET';
-
-        $client = new BlocktrailSDK($apiKey, $apiSecret, $network, $testnet, $apiVersion, $apiEndpoint);
-
-        return $client;
-    }
-
-    protected function getRandomTestIdentifier() {
-        $identifier = md5(openssl_random_pseudo_bytes(128));
-        $time = time();
-
-        return "php-sdk-{$time}-{$identifier}";
-    }
-
-    /**
-     * initial setup to create the wallets that we use
-     */
-    public function testSetup() {
-        // $client = $this->setupBlocktrailSDK();
-
-        // wallet used for testing sending a transaction
-        //  - we reuse this wallet because doing discovery everytime is a pain
-        // $this->createTransactionTestWallet($client);
-
-        // wallet used for
-        //  * testing keyIndex upgrade
-        //  * discovery
-        //  * bad password
-        //  - we recreate this wallet everytime because we need to be able to upgrade it again
-        //  - so it should contain a little bit of BTC to be found by discovery
-        // $this->createDiscoveryTestWallet($client, "unittest-discovery");
-
-        // wallet used for wallet recovery testing
-        //  - we recreate this wallet everytime because we need to be able to upgrade it again
-        //  - so it should contain a little bit of BTC to be found by discovery
-        //    it should be spread across small gaps to test the recovery process
-        // $this->createRecoveryTestWallet($client);
-    }
-
-    protected function createDiscoveryTestWallet(BlocktrailSDKInterface $client, $identifier, $passphrase = "password") {
-        $primaryMnemonic = "give pause forget seed dance crawl situate hole kingdom";
-        $backupMnemonic = "give pause forget seed dance crawl situate hole course";
-
-        return $this->_createTestWallet($client, $identifier, $passphrase, $primaryMnemonic, $backupMnemonic);
-    }
-
-    protected function createTransactionTestWallet(BlocktrailSDKInterface $client, $identifier = "unittest-transaction") {
-        $primaryMnemonic = "give pause forget seed dance crawl situate hole keen";
-        $backupMnemonic = "give pause forget seed dance crawl situate hole give";
-        $passphrase = "password";
-
-        return $this->_createTestWallet($client, $identifier, $passphrase, $primaryMnemonic, $backupMnemonic);
-    }
-
-    protected function createRecoveryTestWallet(BlocktrailSDKInterface $client, $identifier = "unittest-wallet-recovery") {
-        $primaryMnemonic = "give pause forget seed dance crawl situate hole join";
-        $backupMnemonic = "give pause forget seed dance crawl situate hole crater";
-        $passphrase = "password";
-
-        return $this->_createTestWallet($client, $identifier, $passphrase, $primaryMnemonic, $backupMnemonic);
-    }
-
-    protected function _createTestWallet(BlocktrailSDKInterface $client, $identifier, $passphrase, $primaryMnemonic, $backupMnemonic, $readOnly = false) {
-        $walletPath = WalletPath::create(9999);
-
-        $secret = "ffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff";
-        $encryptedSecret = CryptoJSAES::encrypt($secret, $passphrase);
-
-        // still using BIP39 to get seedhex to keep all fixtures the same
-        $seed = (new Bip39SeedGenerator())->getSeed($primaryMnemonic, $passphrase);
-        $primaryPrivateKey = BIP32Key::create(HierarchicalKeyFactory::fromEntropy($seed), "m");
-
-        $primaryPublicKey = $primaryPrivateKey->buildKey((string)$walletPath->keyIndexPath()->publicPath());
-        $encryptedPrimarySeed = CryptoJSAES::encrypt(base64_encode($seed->getBinary()), $secret);
-
-        // still using BIP39 to get seedhex to keep all fixtures the same
-        $backupPrivateKey = BIP32Key::create(HierarchicalKeyFactory::fromEntropy((new Bip39SeedGenerator())->getSeed($backupMnemonic, "")), "m");
-        $backupPublicKey = $backupPrivateKey->buildKey("M");
-
-        $testnet = true;
-
-        $checksum = $primaryPrivateKey->publicKey()->getAddress()->getAddress();
-
-        $result = $client->storeNewWalletV2(
-            $identifier,
-            $primaryPublicKey->tuple(),
-            $backupPublicKey->tuple(),
-            $encryptedPrimarySeed,
-            $encryptedSecret,
-            false,
-            $checksum,
-            9999
-        );
-
-        $blocktrailPublicKeys = $result['blocktrail_public_keys'];
-        $keyIndex = $result['key_index'];
-
-        $wallet = new WalletV2(
-            $client,
-            $identifier,
-            $encryptedPrimarySeed,
-            $encryptedSecret,
-            [$keyIndex => $primaryPublicKey],
-            $backupPublicKey,
-            $blocktrailPublicKeys,
-            $keyIndex,
-            'bitcoin',
-            $testnet,
-            false,
-            $checksum
-        );
-
-        if (!$readOnly) {
-            $wallet->unlock(['password' => $passphrase]);
-        }
-
-        return $wallet;
-    }
+class WalletTest extends WalletTestCase {
 
     public function testBIP32() {
         $masterkey = HierarchicalKeyFactory::fromExtended("tpubD9q6vq9zdP3gbhpjs7n2TRvT7h4PeBhxg1Kv9jEc1XAss7429VenxvQTsJaZhzTk54gnsHRpgeeNMbm1QTag4Wf1QpQ3gy221GDuUCxgfeZ");
