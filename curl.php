fa4c04856177e63d20c306cc4ab79c74e96e8614
Thomas Kerin <thomas.kerin@bitmaintech.com>
2018-09-24 16:24:47 +0200

    debug failure

--- curl.php
+++ curl.php
@@ -2,6 +2,19 @@
 
 $ch = curl_init();
 curl_setopt($ch, CURLOPT_URL, "https://wallet-api.btc.com");
+curl_setopt($ch, CURLOPT_VERBOSE, true);
+$verbose = fopen('php://temp', 'w+');
+curl_setopt($handle, CURLOPT_STDERR, $verbose);
+
 $output = curl_exec($ch);
+if ($result === FALSE) {
+    printf("cUrl error (#%d): %s<br>\n", curl_errno($handle),
+           htmlspecialchars(curl_error($handle)));
+} else {
+    echo $output;
+}
+rewind($verbose);
+$verboseLog = stream_get_contents($verbose);
+
+echo "Verbose information:\n<pre>", htmlspecialchars($verboseLog), "</pre>\n";
 curl_close($ch);
-echo $output;
